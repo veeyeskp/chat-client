26874fd79bb1b6fc0365411072da80b51409be8e
veeyeskp <45894209+veeyeskp@users.noreply.github.com>
2025-06-15 19:40:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <div>
+      <h2>Chat with PDF</h2>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
