ce2b770f4ddfb7492d4ee99dc3b808102af25a61
veeyeskp <45894209+veeyeskp@users.noreply.github.com>
2025-06-22 15:11:52 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import Chat from "./Chat";
+import React from 'react';
+import Chat from './components/Chat';
 
 function App() {
   return (
     <div>
-      <h2>Chat with PDF</h2>
       <Chat />
     </div>
   );
